4a2a6c88815db28baf41781f90e8a672a14d0523
khanhungne <mamhung1110@gmail.com>
2024-12-02 21:49:33 +0700

    Apply consistent line endings across the project

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("111")
+const express = require('express');
+const connectDB = require('./config/db');
+require('dotenv').config();
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+module.exports = app;
