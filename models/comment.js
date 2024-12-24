69f746814c564f5504874097c536b8787e422e25
khanhungne <mamhung1110@gmail.com>
2024-12-24 23:46:33 +0700

    collection post, like, cmt

--- models/comment.js
+++ models/comment.js
@@ -1,1 +1,37 @@
- 
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+    {
+        postId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post',
+            required: true,
+        },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        parentCommentId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment',
+            default: null,
+        },
+        nestedLevel: {
+            type: Number,
+            default: 1,
+        },
+        likesCount: {
+            type: Number,
+            default: 0,
+        },
+    },
+    { timestamps: true }
+);
+
+const Comment = mongoose.model('Comment', commentSchema);
+module.exports = Comment;
--- models/like.js
+++ models/like.js
@@ -1,1 +1,26 @@
- 
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        postId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Post',
+            default: null, // Tuỳ chọn: Like trên bài viết
+        },
+        commentId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment',
+            default: null, // Tuỳ chọn: Like trên comment
+        },  
+    },
+    { timestamps: true }
+);
+
+const Like = mongoose.model('Like', likeSchema);
+
+module.exports = Like;
