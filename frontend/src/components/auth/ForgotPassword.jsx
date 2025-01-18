36246e0fa4f550a2d50c325242feaeaff761b84e
khanhhung <mamhung1110@gmail.com>
2025-01-18 09:24:30 +0700

    update

---
+++ frontend/src/components/auth/ForgotPassword.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import {TextField, Button, Box} from '@mui/material';
+export default function ForgotPassword() {
+    return(
+        <h1></h1>
+    );
+}
