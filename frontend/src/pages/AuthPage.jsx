36ba559ea497c40781bc55a0b8c0113aa12b08da
khanhhung <mamhung1110@gmail.com>
2025-01-18 09:38:07 +0700

    set up temp folder

---
+++ frontend/src/pages/AuthPage.jsx
@@ -0,0 +1,10 @@
+import LoginCard from "../components/auth/LoginCard";
+import SignupCard from "../components/auth/SignupCard";
+
+const AuthPage = () => {
+    return(
+<h1>hahah</h1>    
+);
+}
+
+export default AuthPage;
